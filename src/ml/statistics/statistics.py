23fda7c2a7aa06fd9716b82d3f02ad1361f08caf
Rafael Barreto <rafaelbarreto87@gmail.com>
2011-07-02 09:32:24 -0300

    some minor fix in the documentation fo statistics.py

--- src/ml/statistics/statistics.py
+++ src/ml/statistics/statistics.py
@@ -1,6 +1,46 @@
 # -*- coding: utf-8 -*-
 
 
+def bincount(data, weights=None):
+    """Measures the weighted frequency of the data elements.
+
+    Parameters
+    ----------
+    data : flat list or 1D-array
+        The data.
+    weights : flat list or 1D-array, optional
+        Weights for the data. It must be an array the same size as the data.
+        weights[i] measures the contribution of data[i]. When not specified,
+        it's assumed that weights[i] == 1 for all i.
+
+    Returns
+    -------
+    counts : dict
+        A mapping between a data element and its frequency.
+
+    Examples
+    --------
+
+    Without weights:
+
+    >>> bincount([1, 3, 4, 4, 5, 4, 3, 2, 3, 4, 2, 2, 3])
+    ... {1: 1, 2: 3, 3: 4, 4: 4, 5: 1}
+
+    With weights:
+
+    >>> bincount([1, 3, 4, 4], [0.5, 2, 1, 1])
+    ... {1: 0.5, 3: 2, 4: 2}
+    >>> bincount([1, 3, 4, 4, 4], [0.5, 2, 1, 0.5, 1])
+    ... {1: 0.5, 3: 2, 4: 2.5}
+    """
+
+    if weights is None:
+        weights = [1] * len(data)
+    counts = {}
+    for i in xrange(len(data)):
+        counts[data[i]] = counts.get(data[i], 0) + weights[i]
+    return counts
+
 def mode(data, weights=None):
     """Returns a list of the modal values in the data and the respective
     count.
@@ -10,9 +50,9 @@ def mode(data, weights=None):
     data : flat list or 1D-array
         The data.
     weights : flat list or 1D-array, optional
-        Weights for the data. It must be an array with the same size of the
-        data. weights[i] measures the contribution of data[i]. When weights
-        are not specified, it's assumed that weights[i] == 1 for all i.
+        Weights for the data. It must be an array the same size as the data.
+        weights[i] measures the contribution of data[i]. When not specified,
+        it's assumed that weights[i] == 1 for all i.
 
     Returns
     -------
@@ -45,43 +85,3 @@ def mode(data, weights=None):
     max_count = max(counts.values())
     mode_list = list(set(k for k, v in counts.items() if v==max_count))
     return mode_list, max_count
-
-def bincount(data, weights=None):
-    """Measures the weighted frequency of the data elements.
-
-    Parameters
-    ----------
-    data : flat list or 1D-array
-        The data.
-    weights : flat list or 1D-array, optional
-        Weights for the data. It must be an array with the same size of the
-        data. weights[i] measures the contribution of data[i]. When weights
-        are not specified, it's assumed that weights[i] == 1 for all i.
-
-    Returns
-    -------
-    counts : dict
-        A mapping between a data element and its frequency.
-
-    Examples
-    --------
-
-    Without weights:
-
-    >>> bincount([1, 3, 4, 4, 5, 4, 3, 2, 3, 4, 2, 2, 3])
-    ... {1: 1, 2: 3, 3: 4, 4: 4, 5: 1}
-
-    With weights:
-
-    >>> bincount([1, 3, 4, 4], [0.5, 2, 1, 1])
-    ... {1: 0.5, 3: 2, 4: 2}
-    >>> bincount([1, 3, 4, 4, 4], [0.5, 2, 1, 0.5, 1])
-    ... {1: 0.5, 3: 2, 4: 2.5}
-    """
-
-    if weights is None:
-        weights = [1] * len(data)
-    counts = {}
-    for i in xrange(len(data)):
-        counts[data[i]] = counts.get(data[i], 0) + weights[i]
-    return counts
